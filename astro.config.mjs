01ccf72acde00ce545fb685919d21b434149d679
DJ Neill <dj.neill856@gmail.com>
2024-03-09 16:27:53 -0600

    robot.txt & sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://djneill.dev/',
+  integrations: [tailwind(), robotsTxt({
+    sitemap: ['https://djneill.dev/sitemap-index.xml'],
+  })]
 });
